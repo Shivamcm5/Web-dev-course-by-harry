a4a2be35b0e08959fdfa0ba577348b51a071ad7b
Shivamcm5 <146983725+Shivamcm5@users.noreply.github.com>
2024-10-22 21:31:15 +0530

    new

--- JavaScript_work/Video_57/script.js
+++ JavaScript_work/Video_57/script.js
@@ -1,6 +1,16 @@
 console.log("Hello, Welcome for loop")
 console.log(1)
+
+{ // Example for "For" loop
 let a = 1;
+
 for (let i = 0; i < 10000; i++) {
     console.log(a + i);
+}
+}
+
+{
+let a = {
+    name: "Shivam"
+}
 }
