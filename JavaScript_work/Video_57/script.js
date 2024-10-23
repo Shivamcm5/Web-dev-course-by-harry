e921f99aafcf66c16187a1a67260e288a7314712
Shivamcm5 <146983725+Shivamcm5@users.noreply.github.com>
2024-10-23 17:30:29 +0530

    new

--- JavaScript_work/Video_57/script.js
+++ JavaScript_work/Video_57/script.js
@@ -2,15 +2,40 @@ console.log("Hello, Welcome for loop")
 console.log(1)
 
 { // Example for "For" loop
-let a = 1;
+    let a = 1;
 
-for (let i = 0; i < 10000; i++) {
-    console.log(a + i);
-}
+    for (let i = 0; i < 1; i++) {
+        console.log(a + i);
+    }
 }
 
-{
-let a = {
-    name: "Shivam"
+{ // Example for "Forin" loop  
+    let a = {
+        Name: "Shivam",
+        Age: 14,
+        "current role": "Nothing 2nd option",
+        "Wanna be": "Programmer or Crickter",
+    }
+
+    for (const key in a) {
+        const element = a[key]
+        console.log(key, element)
+    }
 }
+
+{ // Example of "While" loop
+    let a = 2;
+
+    while (a < 5) {
+        console.log(a)
+        a++
+    }
+}
+
+{ // Example of "Do while" loop
+    let a = 3
+    do {
+        console.log(a)
+        a++
+    } while (a<2);
 }
